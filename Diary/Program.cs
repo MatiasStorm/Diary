6eae263765d242f8f6e96a5fd43e4441e6008959
MatiasStorm <48760011+MatiasStorm@users.noreply.github.com>
2019-08-08 18:18:05 +0200

    Planning and initialized the making of the function selection interface

--- Diary/Program.cs
+++ Diary/Program.cs
@@ -4,12 +4,91 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* TODO:
+    - Create a console option selection class, which can return the selected value.
+    - Create a diary class for writting in the diary
+    - Create a file class for opening, writting and searching files.
+ 
+     
+     */
+
 namespace Diary
 {
     class Program
     {
         static void Main(string[] args)
         {
+            List<string> functions = new List<string>();
+            functions.Add("New File");
+            functions.Add("Search Files");
+            functions.Add("Edit Files");
+
+            Console.Clear();
+            foreach(string f in functions)
+            {
+                Console.WriteLine(f);
+            }
+            //Console.WriteLine();
+            //Console.Write("input: ");
+
+            var originalpos = Console.CursorTop;
+
+            var k = Console.ReadKey();
+            var conIndex = 1;
+            var funcIndex = functions.Count;
+
+            while (k.KeyChar != 'q')
+            {
+
+                if (k.Key == ConsoleKey.UpArrow)
+                {
+                    funcIndex--;
+
+                    Console.SetCursorPosition(0, Console.CursorTop - conIndex);
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.WriteLine(functions.ElementAt(funcIndex));
+                    Console.ResetColor();
+                    Console.SetCursorPosition(7, originalpos);
+                    Console.Write(funcIndex);
+                    Console.SetCursorPosition(0, Console.CursorTop - conIndex);
+
+                    conIndex++;
+
+                }
+                if(k.Key == ConsoleKey.Enter)
+                {
+                    Console.SetCursorPosition(0, originalpos + 1);
+                    Console.WriteLine(functions.ElementAt(funcIndex));
+
+                }
+
+                Console.SetCursorPosition(8, originalpos);
+                k = Console.ReadKey();
+            }
+
+
+            //Console.WriteLine("Hello welcome to you Diary Program");
+            //Console.Write("Select a function: n for New File, s for Search, v for View excisting files");
+            //Console.ReadKey();
+            //Console.WriteLine("How was your day");
+
+            //Console.ReadKey
+        }
+
+        static void Write()
+        {
+
+        }
+
+        static void Search()
+        {
+
+        }
+
+        static void Edit()
+        {
+
         }
     }
 }
